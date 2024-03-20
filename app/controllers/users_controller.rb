46f449323adeb96d42c8b4ccc68f3f0f3f168ef5
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-20 09:02:03 -0700

    fix: Make sure spacing in UsersController is 2 instead of 3 and indent correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,26 @@
 class UsersController < ApplicationController
-
-   def new
-      @user = User.new
-   end
-
-   def show
-      @user = User.find(params[:id])
-   end
-
-   def create
-      user = User.new(user_params)
-      if user.save
-         flash[:success] = 'Successfully Created New User'
-         redirect_to user_path(user)
-      else
-         flash[:error] = "#{error_message(user.errors)}"
-         redirect_to register_user_path
-      end
-   end
-
-
-
-
-private
-
-  def user_params
-      params.require(:user).permit(:name, :email)
+  def new
+    @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = 'Successfully Created New User'
+      redirect_to user_path(user)
+    else
+      flash[:error] = "#{error_message(user.errors)}"
+      redirect_to register_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
