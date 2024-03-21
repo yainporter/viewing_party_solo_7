37be754da88631b5e3570038f02c8453b5a9cbfe
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-03-21 13:49:22 -0700

    feat: Add optional parameter include_hidden: false to collection_check_boxes to remove empty String

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -29,15 +29,11 @@ class ViewingPartyController < ApplicationController
   end
 
   def create_user_parties(viewing_party)
-    filter_user_party_ids.map{ |user_id| viewing_party.user_parties.create(user_id:, host: false) }
+    viewing_party.user_parties.map{ |user_id| viewing_party.user_parties.create(user_id:, host: false) }
     viewing_party.user_parties.create!(user_id: params[:user_id], host: true)
   end
 
   def user_party_ids
     params.require(:viewing_party).permit(user_ids: [])
-  end
-
-  def filter_user_party_ids
-    user_party_ids[:user_ids].reject { |id| id == "" }
   end
 end
